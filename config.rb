449065b0933f0c9acc717c0be6542f7ae90fa066
Mike Ball <mikedball@gmail.com>
2014-11-01 08:03:44 -0400

    removed references to 'mass-drowning'

--- config.rb
+++ config.rb
@@ -20,7 +20,7 @@ Fog.credentials = { path_style: true }
 
 activate :sync do |sync|
   sync.fog_provider = 'AWS'
-  sync.fog_directory = 'lost.mikeball.info'
+  sync.fog_directory = 'TODO'
   sync.fog_region = 'us-east-1'
   sync.aws_access_key_id = ENV['AWS_ACCESS_KEY_ID']
   sync.aws_secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
